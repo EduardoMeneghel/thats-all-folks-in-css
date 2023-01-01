59aa567545bedd3085a6ffcd8277d58908a43b3c
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2022-12-31 23:34:29 -0300

    feat/adicionado botão para dar play no audio e animação

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-const intro = document.querySelector('.font');
-intro.style.opacity = 0;
-setTimeout(() => typeWriter(intro), 1000);
 
-function typeWriter(elemento) {
-    intro.style.opacity = 1;
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    textoArray.forEach((letra, i) => {
-        setTimeout(() => elemento.innerHTML += letra, 100 * i);
-        typeWriter(intro)
-    });
-}
+ function start() {
+    document.getElementById('estilo').href = "./css/style.css"
+    document.getElementById('audio').play();
+
+    function typeWriter(elemento) {
+        intro.style.opacity = 1;
+        const textoArray = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        textoArray.forEach((letra, i) => {
+            setTimeout(() => elemento.innerHTML += letra, 100 * i);
+            typeWriter(intro)
+        });
+    }
+    const intro = document.querySelector('.font');
+        intro.style.opacity = 0;
+        const start = document.querySelector('.start');
+        start.style.opacity = 0;
+        setTimeout(() => typeWriter(intro), 1000);
+    
+};
+
